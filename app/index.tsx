6fe34bb13ed9d7365896f25c328d5180f4cf05f0
Maria Santos <mfqsantosz@gmail.com>
2026-02-08 21:48:06 -0300

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View, Image } from "react-native";
+import { StatusBar } from "expo-status-bar"
+import '../global.css'
+
+export default function HomeScreen() {
+  return (
+    <View>
+    </View>
+  );
+}
